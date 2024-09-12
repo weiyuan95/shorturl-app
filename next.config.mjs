7edb5ec0aa94e23d8452e00fc496680a07152a3b
Lee Wei Yuan <35588002+weiyuan95@users.noreply.github.com>
2024-09-12 17:58:25 +0800

    chore: Implement CSP (#2)
    * chore: Implement CSP
    
    * more options

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,43 @@
+const isDevEnv = process.env.NODE_ENV === 'development';
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-inline' ${isDevEnv ? "'unsafe-eval'" : "'url.weiyuan.dev'"};
+    style-src 'self' 'unsafe-inline';
+    img-src 'self';
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    connect-src 'self' ${isDevEnv ? 'localhost:3000' : 'api.url.weiyuan.dev'};
+    upgrade-insecure-requests;
+`;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
